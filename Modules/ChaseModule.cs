3b37ebc5aeb63fcf47502958fa3bd0a20cbb598b
James Aubin <jaubin9422@gmail.com>
2019-07-14 14:51:55 -0500

    added chase command & copypastas

---
+++ Modules/ChaseModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace TerminusDotNetConsoleApp.Modules
+{
+    class ChaseModule : ModuleBase<SocketCommandContext>
+    {
+        
+        [Command("chase")]
+        [Summary("Echoes a spicy chase quote.")]
+        public async Task SayAsync()
+        {
+            var random = new Random();
+            var nikkPastas = File.ReadAllLines(@"RandomMessages\chase.txt");
+            await Context.Channel.SendMessageAsync(nikkPastas[random.Next(nikkPastas.Length)]);
+        }
+    }
+}
