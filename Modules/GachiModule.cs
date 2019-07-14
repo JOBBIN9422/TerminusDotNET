439acdd14e5d5b2159f7f4c4f16489bd98c41b14
James Aubin <jaubin9422@gmail.com>
2019-07-14 15:10:02 -0500

    added gachi command module

---
+++ Modules/GachiModule.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TerminusDotNetConsoleApp.Modules
+{
+    public class GachiModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("gachi")]
+        [Summary("Echoes a spicy gachi quote.")]
+        public async Task SayAsync()
+        {
+            var random = new Random();
+            var gachiPastas = File.ReadAllLines(@"RandomMessages\gachi.txt");
+            await Context.Channel.SendMessageAsync(gachiPastas[random.Next(gachiPastas.Length)]);
+        }
+    }
+}
