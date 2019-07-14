21fe8cdf96bde1cb4bc095d7ff7f4d8d604bf45b
James Aubin <jaubin9422@gmail.com>
2019-07-14 15:00:45 -0500

    added JC command module

---
+++ Modules/JCModule.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TerminusDotNetConsoleApp.Modules
+{
+    public class JCModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("JC")]
+        [Summary("Echoes a spicy JC quote.")]
+        public async Task SayAsync()
+        {
+            var random = new Random();
+            var jcPastas = File.ReadAllLines(@"RandomMessages\jc.txt");
+            await Context.Channel.SendMessageAsync(jcPastas[random.Next(jcPastas.Length)]);
+        }
+    }
+}
