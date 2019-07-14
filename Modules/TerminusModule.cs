4f2bf91540da128404c7e329c40b032cad4dc9b1
James Aubin <jaubin9422@gmail.com>
2019-07-14 15:21:44 -0500

    added terminus command module

---
+++ Modules/TerminusModule.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TerminusDotNetConsoleApp.Modules
+{
+    public class TerminusModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("terminus")]
+        [Summary("Echoes a spicy terminus quote.")]
+        public async Task SayAsync()
+        {
+            var random = new Random();
+            var terminusPastas = File.ReadAllLines(@"RandomMessages\terminus.txt");
+            await Context.Channel.SendMessageAsync(terminusPastas[random.Next(terminusPastas.Length)]);
+        }
+    }
+}
