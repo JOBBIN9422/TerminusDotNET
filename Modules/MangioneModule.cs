ae9b135763e7530a62ffcfffb2ccdde9bffd072b
Michael Hall <mhvhall@gmail.com>
2019-12-07 11:41:01 -0600

    build rudimentary song queueing system - this is kind of broken atm for reasons unknonw

--- Modules/MangioneModule.cs
+++ Modules/MangioneModule.cs
@@ -53,5 +53,73 @@ namespace TerminusDotNetCore.Modules
             await _service.SendAudioAsync(Context.Guild, path, command);
             await _service.LeaveAudio(Context.Guild);
         }
+
+        [Command("play", RunMode = RunMode.Async)]
+        [Summary("Play a song of your choice in an audio channel of your choice (defaults to verbal shitposting)\nSong aliases are: \"mangione1\"")]
+        public async Task PlaySong(string song, string channelID = "-1")
+        {
+            //check if channel id is valid and exists
+            ulong voiceID;
+            IConfiguration config = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json", true, true)
+                                        .Build();
+            if ( channelID.Equals("-1") )
+            {
+                voiceID = ulong.Parse(config["AudioChannelId"]);
+            }
+            else
+            {
+                try
+                {
+                    voiceID = ulong.Parse(channelID);
+                }
+                catch (Exception e)
+                {
+                    await ReplyAsync("Unable to parse channel ID, try letting it use the default");
+                    return;
+                }
+            }
+            if ( Context.Guild.GetVoiceChannel(voiceID) == null )
+            {
+                await ReplyAsync("Invalid channel ID, try letting it use the default");
+                return;
+            }
+            //check if path is valid and exists
+            // TODO check if file type can be played (mp3, wav, idk what ffmpeg can play)
+            string path = "assets/";
+            switch ( song )
+            {
+                case "mangione1":
+                    path += "feels_so_good.mp3";
+                    break;
+                default:
+                    path += song;
+                    break;
+            }
+            path = Path.GetFullPath(path);
+            if (!File.Exists(path))
+            {
+                await ReplyAsync("File does not exist.");
+                return;
+            }
+            await _service.QueueSong(Context.Guild, path, voiceID, config["FfmpegCommand"]);
+        }
+
+        [Command("join", RunMode = RunMode.Async)]
+        public async Task joinChannel(int num = 1)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json", true, true)
+                                        .Build();
+            ulong voiceID = ulong.Parse(config["AudioChannelId"]);
+            if( num == 2) { voiceID = ulong.Parse(config["WeedChannelId"]); }
+            await _service.JoinAudio(Context.Guild, Context.Guild.GetVoiceChannel(voiceID));
+        }
+
+        [Command("leave", RunMode = RunMode.Async)]
+        public async Task leaveChannel(int num = 1)
+        {
+            await _service.LeaveAudio(Context.Guild);
+        }
     }
 }
