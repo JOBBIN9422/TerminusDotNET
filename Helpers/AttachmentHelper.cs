e8a1efc029d200b8aa5804eac8267446454fdbcf
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2019-12-10 08:30:30 -0600

    move GetAttachments into AttachmentHelper
    added filter options for GetAttachments

--- Helpers/AttachmentHelper.cs
+++ Helpers/AttachmentHelper.cs
@@ -5,8 +5,103 @@ using System.Net;
 
 namespace TerminusDotNetCore.Helpers
 {
+    public enum AttachmentFilter
+    {
+        Images,
+        Audio
+    }
+    
     public static class AttachmentHelper
     {
+        private static readonly string[] _validImageExtensions = {
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".bmp",
+            ".gif"
+        };
+
+        private static readonly string[] _validAudioExtensions = {
+            ".mp3"
+        };
+
+        public async static Task<IReadOnlyCollection<Attachment>> GetAttachmentsAsync(SocketCommandContext context, AttachmentFilter filter)
+        {
+            //choose the array containing the proper file extensions to filter by 
+            string[] validExtensions = new string[] { };
+            switch (filter)
+            {
+                case AttachmentFilter.Audio:
+                    validExtensions = _validAudioExtensions;
+                    break;
+
+                case AttachmentFilter.Images:
+                    validExtensions = _validImageExtensions;
+                    break;
+
+                default:
+                    validExtensions = _validImageExtensions;
+                    break;
+            }
+
+            var attachments = context.Message.Attachments;
+            if (attachments == null || attachments.Count == 0)
+            {
+                //check the last 20 messages for attachments (from most recent to oldest)
+                var messages = await context.Channel.GetMessagesAsync(20).FlattenAsync();
+                foreach (var message in messages)
+                {
+                    if (message.Attachments.Count > 0)
+                    {
+                        //skip this message if its attachments don't pass the filter
+                        if (!AttachmentsAreValid(message.Attachments, validExtensions))
+                        {
+                            continue;
+                        }
+
+                        //return attachments if they're all valid
+                        else
+                        {
+                            return (IReadOnlyCollection<Attachment>)message.Attachments;
+                        }
+                    }
+                }
+
+                //if none of the previous messages had any attachments
+                throw new NullReferenceException("No attachments were found in the current or previous messages.");
+            }
+            else
+            {
+                if (AttachmentsAreValid(attachments, validExtensions))
+                {
+                    return attachments;
+                }
+                else
+                {
+                    throw new NullReferenceException("No attachments were found in the current or previous messages.");
+                }
+            }
+        }
+
+        private static bool AttachmentsAreValid(IReadOnlyCollection<IAttachment> attachments, string[] validExtensions)
+        {
+            foreach (var attachment in attachments)
+            {
+                if (!FileIsValid(attachment.Filename, validExtensions))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool FileIsValid(string filename, string[] validExtensions)
+        {
+            string extension = Path.GetExtension(filename);
+            return Array.Exists(validExtensions, element => element == extension);
+        }
+        
         public static List<string> DownloadAttachments(IReadOnlyCollection<Attachment> attachments, string downloadPath = "")
         {
             using (var webClient = new WebClient())
