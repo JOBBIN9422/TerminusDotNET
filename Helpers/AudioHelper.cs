fe208e4ace33a1484ea13b82fd88449aeecbe8da
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2019-12-10 10:13:03 -0600

    add youtube stream process

--- Helpers/AudioHelper.cs
+++ Helpers/AudioHelper.cs
@@ -1,6 +1,7 @@
 using Discord;
 using Discord.Commands;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.IO;
@@ -10,5 +11,17 @@ namespace TerminusDotNetCore.Helpers
 {
     public class AudioHelper
     {
+        public static Process CreateYTStreamProcess(string path)
+        {
+            //idk if this is going to work
+            return Process.Start(new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"/C youtube-dl.exe -o - {path} | ffmpeg -i pipe:0 -ac 2 -f s16le -ar 48000 pipe:1",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                });
+        }
     }
 }
