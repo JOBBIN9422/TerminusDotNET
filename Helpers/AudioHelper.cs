83318ecf8cb9a16df6d5a8e61905763d3201f8c6
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2019-12-10 09:39:20 -0600

    Create AudioHelper.cs

---
+++ Helpers/AudioHelper.cs
@@ -0,0 +1,14 @@
+using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace TerminusDotNetCore.Helpers
+{
+    public class AudioHelper
+    {
+    }
+}
