9bd01e5cfb4415c9d598bff59116b99a934b3db1
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2019-12-10 10:17:27 -0600

    add audiotype enum

--- Helpers/AudioItem.cs
+++ Helpers/AudioItem.cs
@@ -10,9 +10,21 @@ using System.Net;
 
 namespace TerminusDotNetCore.Helpers
 {
+    public enum AudioType
+    {
+        Local,
+        YouTube
+    }
+    
     public class AudioItem
     {
+        //for local files: the filename. for streamed audio: the youtube URL.
         public string Path { get; set; }
+        
+        //channel ID to play this item in
         public ulong PlayChannelId { get; set; }
+        
+        //where does the audio originate? (local file or streamed from youtube)
+        public AudioType AudioSource { get; set; }
     }
 }
