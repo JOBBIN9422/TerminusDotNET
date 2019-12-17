b4bd895734281eb02c5e28b7bd8849d8deec03cd
root <root@guest.guest>
2019-12-17 15:52:14 -0600

    add base classes

---
+++ Modules/ServiceControlModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerminusDotNetCore.Services;
+
+namespace TerminusDotNetCore.Modules
+{
+    public class ServiceControlModule
+    {
+        //allow services to reply on a text channel
+        //Task ServiceReplyAsync(string s, EmbedBuilder embedBuilder = null);
+
+	public CustomService Service { get; set; }
+    }
+}
---
+++ Services/CustomService.cs
@@ -0,0 +1,17 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerminusDotNetCore.Modules;
+
+namespace TerminusDotNetCore.Services
+{
+    public class CustomService
+    {
+        ServiceControlModule ParentModule { get; set; }
+
+        //Task ServiceReplyAsync(string s, EmbedBuilder embedBuilder = null);
+    }
+}
