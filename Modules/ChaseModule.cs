41350fa29befc86416e308d659b62f0f9d33c559
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2019-12-18 08:54:57 -0600

    Update ChaseModule.cs

--- Modules/ChaseModule.cs
+++ Modules/ChaseModule.cs
@@ -8,15 +8,16 @@ using Discord.Commands;
 
 namespace TerminusDotNetCore.Modules
 {
-    public class ChaseModule : ModuleBase<SocketCommandContext>, ITextModule
+    public class ChaseModule : TextModule
     {
+        public ChaseModule(Random random) : base(random) {}
+        
         [Command("chase")]
         [Summary("Responds with a random quote from the legendary mercenary programmer, Solid Chase.")]
-        public async Task SayAsync()
+        public override async Task SayAsync()
         {
-            var random = new Random();
             var chasePastas = File.ReadAllLines(Path.Combine("RandomMessages", "chase.txt"));
-            await ReplyAsync(chasePastas[random.Next(chasePastas.Length)]);
+            await ReplyAsync(chasePastas[_random.Next(chasePastas.Length)]);
         }
     }
 }
