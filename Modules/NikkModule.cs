d6c58cb26341056bad479986cdcdb8d15a940739
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2019-12-18 09:03:13 -0600

    Update NikkModule.cs

--- Modules/NikkModule.cs
+++ Modules/NikkModule.cs
@@ -5,15 +5,16 @@ using System.Threading.Tasks;
 
 namespace TerminusDotNetCore.Modules
 {
-    public class NikkModule : ModuleBase<SocketCommandContext>, ITextModule
+    public class NikkModule : TextModule
     {
+        public NikkModule(Random random) : base(random) {}
+        
         [Command("nikk")]
         [Summary("Responds with a quote from the legendary mercenary koder, Nikk Hemp(weed)street.")]
-        public async Task SayAsync()
+        public override async Task SayAsync()
         {
-            var random = new Random();
             var nikkPastas = File.ReadAllLines(Path.Combine("RandomMessages", "nikk.txt"));
-            await ReplyAsync(nikkPastas[random.Next(nikkPastas.Length)]);
+            await ReplyAsync(nikkPastas[_random.Next(nikkPastas.Length)]);
         }
     }
 }
