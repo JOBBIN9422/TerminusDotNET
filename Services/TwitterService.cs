c2b500e07e1b165ae30d7a8e4f825c2ce751ac34
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2020-01-08 10:05:29 -0600

    reply with tweet URL

--- Services/TwitterService.cs
+++ Services/TwitterService.cs
@@ -54,7 +54,7 @@ namespace TerminusDotNetCore.Services
                 var tweet = await _twitterContext.TweetAsync(tweetContent);
                 if (tweet != null)
                 {
-                    return $"Successfully tweeted status {tweet.StatusID}.";
+                    return $"Successfully tweeted status:  https://twitter.com/statuses/{tweet.ID}.";
                 }
                 else
                 {
