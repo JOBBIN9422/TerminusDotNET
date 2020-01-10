c4995963ed9f473f100462c46a538b5e2ea1d3f5
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2020-01-10 15:01:12 -0600

    add image helper class

---
+++ Helpers/ImageHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra;
+using SixLabors.Primitives;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.Fonts;
+using System.Drawing;
+using System.Numerics;
+
+namespace TerminusDotNetCore.Helpers
+{
+    public class ImageHelper
+    {
+    
+    }
+}
