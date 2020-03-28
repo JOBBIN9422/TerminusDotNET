f499435a532f2283790dd95dae9f0b12c73f20be
James Aubin <jaubin9422@gmail.com>
2020-03-28 09:29:20 -0500

    comments

--- Services/ICustomService.cs
+++ Services/ICustomService.cs
@@ -11,6 +11,7 @@ namespace TerminusDotNetCore.Services
 {
     public interface ICustomService
     {
+        //shared config object - set in controlling module constructor (passed via DI)
         public IConfiguration Config { get; set; }
         ServiceControlModule ParentModule { get; set; }
 
