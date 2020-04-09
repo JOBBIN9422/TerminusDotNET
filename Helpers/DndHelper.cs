bfb81ffaaf381cbc5d70f670da030d338a257bac
Brian Polasek <brianjohnpolasek@gmail.com>
2020-04-09 13:54:04 -0500

    Added the DndModule and DndHelper files, allowing users to use the "!roll #d#" command.

---
+++ Helpers/DndHelper.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.IO;
+
+namespace TerminusDotNetCore.Helpers
+{
+    class DndHelper
+    {
+        public static async Task<int> RollDice(int die)
+        {
+            Random random = new Random();
+            int roll_result = random.Next(1, die+1);
+
+            return roll_result;
+        }
+    }
+}
