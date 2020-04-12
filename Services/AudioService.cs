6e7a80922a2c16f216791cb0ff021d6898dc29e1
James Aubin <jaubin9422@gmail.com>
2020-04-12 12:15:42 -0500

    don't leave on join

--- Services/AudioService.cs
+++ Services/AudioService.cs
@@ -117,7 +117,7 @@ namespace TerminusDotNetCore.Services
 
         public async Task JoinAudio(int retryCount = 5)
         {
-            await LeaveAudio();
+            //await LeaveAudio();
             //await Task.Delay(100);
 
             IAudioClient audioClient = null;
