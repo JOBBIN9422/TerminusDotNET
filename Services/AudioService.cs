9009c8029cce4fb730c970f551af4deac9c32265
James Aubin <jaubin9422@gmail.com>
2020-04-28 15:08:00 -0500

    i am dumb

--- Services/AudioService.cs
+++ Services/AudioService.cs
@@ -341,7 +341,7 @@ namespace TerminusDotNetCore.Services
             {
                 await ParentModule.ServiceReplyAsync("There are no songs in the queue.");
             }
-            else if (index > _songQueue.Count - 2 || index < 2)
+            else if (index > _songQueue.Count + 1 || index < 2)
             {
                 await ParentModule.ServiceReplyAsync("The requested index was out of bounds.");
             }
