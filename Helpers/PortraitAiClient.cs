64c16ee3a84aeea79467093d418f5ea04c31ca7f
James Aubin <jaubin9422@gmail.com>
2020-07-11 10:46:00 -0500

    make styles request

--- Helpers/PortraitAiClient.cs
+++ Helpers/PortraitAiClient.cs
@@ -15,6 +15,7 @@ namespace TerminusDotNetCore.Helpers
     {
         private static readonly string PORTRAITAI_BASE_ADDRESS = "https://a7.portrait-ai.com/";
         private static readonly string POST_IMAGE_ADDRESS = $"{PORTRAITAI_BASE_ADDRESS}v1/c/submit-user-image.php";
+        private static readonly string MAKE_STYLES_ADDRESS = $"{PORTRAITAI_BASE_ADDRESS}v1/c/make-styles.php";
         private static readonly string HASH_SUBSTITUTE_PLACEHOLDER = "HASH";
         private static readonly string GET_PORTRAIT_IMAGE_ADDRESS = $"{PORTRAITAI_BASE_ADDRESS}v1/cropped/{HASH_SUBSTITUTE_PLACEHOLDER}/original.jpg";
 
@@ -43,12 +44,24 @@ namespace TerminusDotNetCore.Helpers
             JObject postImageResponseContent = JsonConvert.DeserializeObject<JObject>(await postImageResponse.Content.ReadAsStringAsync());
             string cropHash = postImageResponseContent["crop_hashes"][0].ToString();
 
-            //send the crop hash to get the portrait from the site
-            HttpResponseMessage getPortraitResponse = await _client.GetAsync(GET_PORTRAIT_IMAGE_ADDRESS.Replace(HASH_SUBSTITUTE_PLACEHOLDER, cropHash));
+            //build the content for the make-styles request
+            MultipartFormDataContent makeStylesContent = new MultipartFormDataContent(boundary);
+            StringContent hexContent = new StringContent(cropHash);
+            hexContent.Headers.Add("Content-Disposition", "form-data; name=\"hex\"");
+            makeStylesContent.Add(hexContent);
+            StringContent stylesContent = new StringContent("[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]");
+            hexContent.Headers.Add("Content-Disposition", "form-data; name=\"styles\"");
+            makeStylesContent.Add(stylesContent);
 
-            //overwrite the given image with the data from the response
-            byte[] portraitImageData = await getPortraitResponse.Content.ReadAsByteArrayAsync();
-            await File.WriteAllBytesAsync(imageFilename, portraitImageData);
+            //send the make-styles request
+            HttpResponseMessage makeStylesResponse = await _client.PostAsync(MAKE_STYLES_ADDRESS, makeStylesContent);
+
+            ////send the crop hash to get the portrait from the site
+            //HttpResponseMessage getPortraitResponse = await _client.GetAsync(GET_PORTRAIT_IMAGE_ADDRESS.Replace(HASH_SUBSTITUTE_PLACEHOLDER, cropHash));
+
+            ////overwrite the given image with the data from the response
+            //byte[] portraitImageData = await getPortraitResponse.Content.ReadAsByteArrayAsync();
+            //await File.WriteAllBytesAsync(imageFilename, portraitImageData);
         }
     }
 }
