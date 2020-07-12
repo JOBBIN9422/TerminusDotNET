2ba1684381a107eaa8a933de4a9eaee05c1ff346
James Aubin <jaubin9422@gmail.com>
2020-07-12 13:46:23 -0500

    cleanup

--- Helpers/PortraitAiClient.cs
+++ Helpers/PortraitAiClient.cs
@@ -19,6 +19,7 @@ namespace TerminusDotNetCore.Helpers
 
         private static readonly int MAX_RETRY_COUNT = 20;
 
+        private static string STYLES_LIST = "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]";
         private static string HASH_SUBSTITUTE_PLACEHOLDER = "HASH";
         private static string STYLE_INDEX_PLACEHOLDER = "STYLE";
         private static string PORTRAITAI_BASE_ADDRESS = $"https://a{_random.Next(1, 16)}.portrait-ai.com/";
@@ -40,6 +41,7 @@ namespace TerminusDotNetCore.Helpers
 
         public static async Task PostImage(string imageFilename)
         {
+            //choose a random subdomain (the site does this internally)
             ReseedUrls();
 
             //determine type of the image to be sent
@@ -58,36 +60,39 @@ namespace TerminusDotNetCore.Helpers
             imageDataContent.Headers.ContentType = MediaTypeHeaderValue.Parse(mimeType);
             imageContent.Add(imageDataContent, "image", Path.GetFileName(imageFilename));
 
-            //extract the crop hash from the response
+            //send the image to be cropped and extract the crop hash from the response
             HttpResponseMessage postImageResponse = await _client.PostAsync(POST_IMAGE_ADDRESS, imageContent);
             JObject postImageResponseContent = JsonConvert.DeserializeObject<JObject>(await postImageResponse.Content.ReadAsStringAsync());
             string cropHash = postImageResponseContent["crop_hashes"][0].ToString();
 
             //build the content for the make-styles request
             MultipartFormDataContent makeStylesContent = new MultipartFormDataContent(boundary);
+
+            //add the crop hash
             StringContent hexContent = new StringContent(cropHash);
             hexContent.Headers.Add("Content-Disposition", "form-data; name=\"hex\"");
             makeStylesContent.Add(hexContent);
-            StringContent stylesContent = new StringContent("[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]");
+
+            //add the possible styles list
+            StringContent stylesContent = new StringContent(STYLES_LIST);
             stylesContent.Headers.Add("Content-Disposition", "form-data; name=\"styles\"");
             makeStylesContent.Add(stylesContent);
 
-            //send the make-styles request
+            //send the make-styles request in order to get available portrait styles to choose from
             HttpResponseMessage makeStylesResponse = await _client.PostAsync(MAKE_STYLES_ADDRESS, makeStylesContent);
 
-            //get styles which are ready for download
             JObject stylesTable = new JObject();
             int requestCount = 0;
             
-            //query which portrait styles are available for download
+            //keep requesting available styles until we get a non-empty response
             do
             {
                 HttpResponseMessage stylesReadyResponse = await _client.GetAsync(STYLES_READY_ADDRESS.Replace(HASH_SUBSTITUTE_PLACEHOLDER, cropHash));
                 requestCount++;
                 string responseContent = await stylesReadyResponse.Content.ReadAsStringAsync();
-                Console.WriteLine(stylesReadyResponse.ToString());
-                Console.WriteLine(responseContent);
                 Thread.Sleep(1000);
+
+                //ignore JSON array responses - almost always empty
                 if (responseContent.StartsWith('['))
                 {
                     continue;
@@ -95,9 +100,10 @@ namespace TerminusDotNetCore.Helpers
                 stylesTable = JsonConvert.DeserializeObject<JObject>(responseContent);
             } while (stylesTable.Count == 0 && requestCount < MAX_RETRY_COUNT);
 
+            //if we didn't get anything after waiting, something is wrong - can't get portraits
             if (stylesTable.Count == 0)
             {
-                throw new TimeoutException($"Could not get a portrait from {PORTRAITAI_BASE_ADDRESS} (likely rate limited).");
+                throw new TimeoutException($"Could not get a portrait from {PORTRAITAI_BASE_ADDRESS} (rate-limited or server-side issue).");
             }
 
             //build a list of style numbers and choose one at random
