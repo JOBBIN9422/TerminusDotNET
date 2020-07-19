b13e70c21ea9fb7494ad9887cf75cd47eccc7a51
James Aubin <jaubin9422@gmail.com>
2020-07-19 13:27:01 -0500

    refactor help to use cmd summary helper methods

--- Modules/HelpModule.cs
+++ Modules/HelpModule.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TerminusDotNetCore.Helpers;
 
 namespace TerminusDotNetCore.Modules
 {
@@ -21,94 +22,18 @@ namespace TerminusDotNetCore.Modules
         [Summary("List information about a given command (if it exists).")]
         public async Task Help([Summary("the name of the command to search for")][Remainder]string commandName = null)
         {
-            //if no command name was given, print help for all commands
-            if (string.IsNullOrEmpty(commandName))
+            List<Embed> helpTexts = CommandSummaryHelper.GenerateHelpEmbeds(_commandService, commandName);
+            if (helpTexts == null)
             {
-                IEnumerable<CommandInfo> commands = _commandService.Commands.OrderBy(c => c.Aliases.First());
-                List<Embed> helpTexts = new List<Embed>();
-                EmbedBuilder embedBuilder = new EmbedBuilder();
-
-                foreach (CommandInfo command in commands)
-                {
-                    //create a new embed if we've hit the field limit for the current embed
-                    if (embedBuilder.Fields.Count % EmbedBuilder.MaxFieldCount == 0 && embedBuilder.Fields.Count > 0)
-                    {
-                        helpTexts.Add(embedBuilder.Build());
-                        embedBuilder = new EmbedBuilder();
-                    }
-
-                    //add a summary field of the current cmd to the current embed
-                    AddCommandField(embedBuilder, command);
-                }
-
-                if (helpTexts.Count == 0 || !helpTexts.Contains(embedBuilder.Build()))
-                {
-                    helpTexts.Add(embedBuilder.Build());
-                }
-
+                await ReplyAsync("No command with the given name was found. (Usage: !help <command-name>)");
+            }
+            else
+            {
                 foreach (var helpEmbed in helpTexts)
                 {
                     await ReplyAsync(embed: helpEmbed);
                 }
             }
-            else
-            {
-                //search for the given command by name
-                SearchResult cmdSearchResult = _commandService.Search(commandName);
-                if (!cmdSearchResult.IsSuccess)
-                {
-                    await ReplyAsync("No command with the given name was found. (Usage: !help <command-name>)");
-                }
-                else
-                {
-                    List<Embed> helpTexts = new List<Embed>();
-                    EmbedBuilder embedBuilder = new EmbedBuilder();
-
-                    foreach (CommandMatch commandMatch in cmdSearchResult.Commands)
-                    {
-                        //create a new embed if we've hit the field limit for the current embed
-                        if (embedBuilder.Fields.Count % EmbedBuilder.MaxFieldCount == 0 && embedBuilder.Fields.Count > 0)
-                        {
-                            helpTexts.Add(embedBuilder.Build());
-                            embedBuilder = new EmbedBuilder();
-                        }
-
-                        //add a summary field of the current cmd to the current embed
-                        AddCommandField(embedBuilder, commandMatch.Command);
-                    }
-
-                    if (helpTexts.Count == 0 || !helpTexts.Contains(embedBuilder.Build()))
-                    {
-                        helpTexts.Add(embedBuilder.Build());
-                    }
-
-                    //send the list of embeds
-                    foreach (var helpEmbed in helpTexts)
-                    {
-                        await ReplyAsync(embed: helpEmbed);
-                    }
-                }
-            }
-        }
-
-        private void AddCommandField(EmbedBuilder embedBuilder, CommandInfo command)
-        {
-            string commandText = command.Summary ?? "No description available.\n";
-
-            IReadOnlyList<ParameterInfo> parameters = command.Parameters;
-
-            //add each command parameter to the help embed
-            foreach (ParameterInfo param in parameters)
-            {
-                //if there is a default value, print it
-                string defaultVal = param.DefaultValue == null ? string.Empty : $", default = `{param.DefaultValue}`";
-                if (param.Summary != null)
-                {
-                    commandText += $"\n- `{param.Name}` (`{param.Type.Name}`, optional = `{param.IsOptional}`{defaultVal}): {param.Summary}";
-                }
-            }
-
-            embedBuilder.AddField($"`{command.Aliases.First()}`", commandText);
         }
     }
 }
