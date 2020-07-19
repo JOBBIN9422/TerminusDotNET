80126e75f5b07b009710799a21db6211737b0fbf
James Aubin <jaubin9422@gmail.com>
2020-07-19 13:02:53 -0500

    error formatting

--- Bot.cs
+++ Bot.cs
@@ -171,8 +171,8 @@ namespace TerminusDotNetCore
             }
             catch (InvalidOperationException)
             {
-                await context.Channel.SendMessageAsync("Unknown command.");
-                await Logger.Log(new LogMessage(LogSeverity.Error, "CommandExecution", $"Unknown command."));
+                await context.Channel.SendMessageAsync($"Unknown command: `{command.Value.Name}`");
+                await Logger.Log(new LogMessage(LogSeverity.Error, "CommandExecution", $"Unknown command: `{command.Value.Name}`"));
             }
         }
 
