fbf46700720c235c2aaa712d23dba018f7328a7a
James Aubin <jaubin9422@gmail.com>
2020-07-19 14:04:18 -0500

    look for nested types

--- Modules/BotManagementModule.cs
+++ Modules/BotManagementModule.cs
@@ -99,7 +99,7 @@ namespace TerminusDotNetCore.Modules
             public async Task PrintUsageInfo()
             {
                 EmbedBuilder embedBuilder = new EmbedBuilder();
-                Attribute[] attributes = Attribute.GetCustomAttributes(typeof(LogModule));
+                Attribute[] attributes = Attribute.GetCustomAttributes(GetType().GetNestedTypes()[0]);
 
                 foreach(Attribute attribute in attributes)
                 {
