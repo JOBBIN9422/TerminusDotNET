dc240c96ca405fab32854a23b4e2a526daf2d8f9
Michael Hall <mhvhall@gmail.com>
2020-12-05 15:12:31 -0600

    Surely this will never error out

--- Modules/LibertyModule.cs
+++ Modules/LibertyModule.cs
@@ -20,14 +20,14 @@ namespace TerminusDotNetCore.Modules
 
         [Command("liberty")]
         [Summary("Activates the Liberty Prime Bot")]
-        public async Task Echo([Summary("Which branch of the liberty repo to pull")] string branch="master")
+        public async Task Liberty([Summary("Which branch of the liberty repo to pull")] string branch="master")
         {
             // Hopefully this will set the right environment variables
             string lib_dir = Config["LibertyPrimeDir"];
             System.Environment.SetEnvironmentVariable("LIBERTY_DIR",lib_dir);
             System.Environment.SetEnvironmentVariable("LIBERTY_BRANCH", branch);
+
             ProcessStartInfo psi = new ProcessStartInfo();
-            // Need to make sure this points to the right spot. Is the root of the repo the working directory?
             psi.FileName = "Automation/activate_liberty_prime.sh";
             psi.UseShellExecute = false;
             psi.RedirectStandardOutput = true;
@@ -35,7 +35,25 @@ namespace TerminusDotNetCore.Modules
             Process p = Process.Start(psi);
             string strOutput = p.StandardOutput.ReadToEnd();
             p.WaitForExit();
-            await ReplyAsync("It either worked or it didn't");
+            await ReplyAsync("Liberty Prime Activated");
+        }
+
+        [Command("kill_liberty")]
+        [Summary("Decommissions Liberty Prime.... For Now")]
+        public async Task KillLiberty()
+        {
+            // In the future, we may have persistent data to be saved from the docker container
+            // If that happens, we should first copy that data out of the container and into
+            // the persistent_files dir. This could happen here or in the deactivate script.
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "Automation/deactivate_liberty_prime.sh";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+
+            Process p = Process.Start(psi);
+            string strOutput = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            await ReplyAsync("Liberty Prime Deactivated");
         }
     }
 }
