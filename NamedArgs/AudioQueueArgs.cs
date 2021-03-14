d3fc25bc573084fb4b31d84392c775ae0678b359
James Aubin <jaubin9422@gmail.com>
2021-03-14 17:50:52 -0500

    add AudioQueuArgs class

---
+++ NamedArgs/AudioQueueArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace TerminusDotNetCore.NamedArgs
+{
+    [NamedArgumentType]
+    public class AudioQueueArgs
+    {
+        //true: enqueue at end (normal behavior)
+        //false: enqueue at front (cut in queue)
+        public bool Append { get; set; }
+        public bool Shuffle { get; set; }
+
+        //channel alias ("main" or "weed")
+        public string Channel { get; set; }
+    }
+}
