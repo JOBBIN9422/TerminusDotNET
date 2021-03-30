1c550ae74af3e11fc872de6aebc1f9c302ddd4fb
leilanihc112 <leilanihc@gmail.com>
2021-03-29 23:05:17 -0500

    testing again

--- Automation/activate_echo_bot.sh
+++ Automation/activate_echo_bot.sh
@@ -8,27 +8,28 @@ set -euo pipefail
 
 # If no dir is specified, fail
 if [ -z "${ECHOBOT_DIR}" ]; then
+	echo "got here"
     exit 1
 fi
 
-# # Working directory should be the echobot dir
-# cd $ECHOBOT_DIR
+# Working directory should be the echobot dir
+cd $ECHOBOT_DIR
 
-# # Check if echobot repo is already here
-# if [ ! -d "Echo-Bot" ]; then
-    # # Need to clone the repo
-    # git clone https://github.com/leilanihc112/Echo-Bot.git
-# fi
+# Check if echobot repo is already here
+if [ ! -d "Echo-Bot" ]; then
+    # Need to clone the repo
+    git clone https://github.com/leilanihc112/Echo-Bot.git
+fi
 
-# # Checkout the selected branch and pull latest
-# # I hope this won't ever fail, but I guess I can fix it if it does
-# cd Echo-Bot
-# git fetch origin $ECHOBOT_BRANCH
-# git checkout $ECHOBOT_BRANCH
-# git pull origin $ECHOBOT_BRANCH
+# Checkout the selected branch and pull latest
+# I hope this won't ever fail, but I guess I can fix it if it does
+cd Echo-Bot
+git fetch origin $ECHOBOT_BRANCH
+git checkout $ECHOBOT_BRANCH
+git pull origin $ECHOBOT_BRANCH
 
-# # Copy over files into echobot dir
-# rsync -r ../persistent_files/ .
+# Copy over files into echobot dir
+rsync -r ../persistent_files/ .
 
-# # Run Echo bot
-# ./run.sh
+# Run Echo bot
+./run.sh
