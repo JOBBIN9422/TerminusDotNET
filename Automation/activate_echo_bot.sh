565decdafe3b209d1b93fcdb5f6f5b309abf60a6
leilanihc112 <leilanihc@gmail.com>
2021-03-29 22:22:28 -0500

    adding echo bot... hopefully

---
+++ Automation/activate_echo_bot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+# This is a script meant to pull in the latest echo bot and run it
+# Expects the ECHOBOT_DIR variable to be exported before running
+
+# If no branch name is exported defualt to master
+: "${ECHOBOT_BRANCH:="master"}"
+
+# If no dir is specified, fail
+if [ -z "${ECHOBOT_DIR}" ]; then
+    exit 1
+fi
+
+# Working directory should be the liberty dir
+cd $ECHOBOT_DIR
+
+# Check if liberty prime repo is already here
+if [ ! -d "echobot" ]; then
+    # Need to clone the repo
+    git clone https://github.com/leilanihc112/Echo-Bot.git
+fi
+
+# Checkout the selected branch and pull latest
+# I hope this won't ever fail, but I guess I can fix it if it does
+cd liberty
+git fetch origin $ECHOBOT_BRANCH
+git checkout $ECHOBOT_BRANCH
+git pull origin $ECHOBOT_BRANCH
+
+# Copy over files into liberty dir
+rsync -r ../persistent_files/ .
+
+# Run Liberty
+./run.sh
