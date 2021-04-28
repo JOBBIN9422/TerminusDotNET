de5cd3ed13cabee911437f3861768c3c5d97f805
James A <jaubin9422@gmail.com>
2021-04-28 15:27:13 -0500

    haha bash injection very cool

--- Services/ServerManagementService.cs
+++ Services/ServerManagementService.cs
@@ -31,13 +31,11 @@ namespace TerminusDotNetCore.Services
         }
         private static async Task<string> RunBashCommand(string cmd, string user)
         {
-            //escape double quotes in cmd
-            string escapedCmd = cmd.Replace("\"", "\\\"");
             using (var bashProcess = Process.Start(
                 new ProcessStartInfo
                 {
                     FileName = "su",
-                    Arguments = $"-s /bin/bash -c \"{escapedCmd}\" {user}",
+                    Arguments = $"-s /bin/bash -c {cmd} {user}",
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
